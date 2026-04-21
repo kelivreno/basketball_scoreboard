7ca021375b0286727b156f63dd46ce73ed770a92
kelivreno <joikonkazama@gmail.com>
2026-04-21 04:56:30 -0700

    add score tracking functionality for home and guest teams

---
+++ index.js
@@ -0,0 +1,40 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+let scoreHome = 0
+let scoreGuest = 0
+
+// Home
+
+function homePlusOne() {
+    scoreHome += 1
+    homeScore.innerText = scoreHome
+}
+
+function homePlusTwo() {
+    scoreHome += 2
+    homeScore.innerText = scoreHome
+}
+
+function homePlusThree() {
+    scoreHome += 3
+    homeScore.innerText = scoreHome
+}
+
+// Guest
+
+function guestPlusOne() {
+    scoreGuest += 1
+    guestScore.innerText = scoreGuest
+}
+
+function guestPlusTwo() {
+    scoreGuest += 2
+    guestScore.innerText = scoreGuest
+}
+
+function guestPlusThree() {
+    scoreGuest += 3
+    guestScore.innerText = scoreGuest
+
+}
