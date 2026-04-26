068c50bdfd40dd8b049f74d42ab679fdf1b84cfd
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-04-26 09:38:37 -0700

    add IDs to home and guest elements for score tracking and update leader functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let homeScore = document.getElementById("home-score")
 let guestScore = document.getElementById("guest-score")
 
+let homeName = document.getElementById("home-el")
+let guestName = document.getElementById("guest-el")
+
+
 let scoreHome = 0
 let scoreGuest = 0
 
@@ -9,16 +13,19 @@ let scoreGuest = 0
 function homePlusOne() {
     scoreHome += 1
     homeScore.innerText = scoreHome
+    updateLeader()
 }
 
 function homePlusTwo() {
     scoreHome += 2
     homeScore.innerText = scoreHome
+    updateLeader()
 }
 
 function homePlusThree() {
     scoreHome += 3
     homeScore.innerText = scoreHome
+    updateLeader()
 }
 
 // Guest
@@ -26,16 +33,19 @@ function homePlusThree() {
 function guestPlusOne() {
     scoreGuest += 1
     guestScore.innerText = scoreGuest
+    updateLeader()
 }
 
 function guestPlusTwo() {
     scoreGuest += 2
     guestScore.innerText = scoreGuest
+    updateLeader()
 }
 
 function guestPlusThree() {
     scoreGuest += 3
     guestScore.innerText = scoreGuest
+    updateLeader()
 
 }
 
@@ -44,4 +54,21 @@ function newGame() {
     homeScore.textContent = scoreGuest
     scoreHome = 0
     guestScore.textContent = scoreHome
+    updateLeader()
+}
+
+function updateLeader() {
+    if (scoreGuest > scoreHome){
+        guestName.style.color = 'red'
+        homeName.style.color = '#EEEEEE'
+    } else if (scoreGuest < scoreHome) {
+        homeName.style.color = 'red'
+        guestName.style.color = '#EEEEEE'
+    } else if (scoreGuest === scoreHome) {
+        homeName.style.color = '#EEEEEE'
+        guestName.style.color = '#EEEEEE'
+    } else  {
+        homeName.style.color = '#EEEEEE'
+        guestName.style.color = '#EEEEEE'
+    }
 }
